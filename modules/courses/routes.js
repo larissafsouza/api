176ee74f8098d2ca8afe025d8b532538d9ac1c85
lfmslarissafabiola <fabiola@meliuz.com.br>
2017-09-11 21:42:09 -0400

    Organiza rotas de disciplinas dentro do diretório de courses

--- modules/courses/routes.js
+++ modules/courses/routes.js
@@ -1,11 +1,13 @@
 var express = require('express');
 
+const averageApprovedSubjectsAction = require('./actions/average_approved_subjects');
 const frequencyPerPeriodAction = require('./actions/frequency_per_period');
 const situationPerPeriodAction = require('./actions/situation_per_period');
 const totalSituationPerPeriodAction = require('./actions/total_situation_per_period');
 
 var router = express.Router();
 
+router.get('/courses/average-approved-subjects', averageApprovedSubjectsAction);
 router.get('/courses/frequency-per-period', frequencyPerPeriodAction);
 router.get('/courses/situation-per-period', situationPerPeriodAction);
 router.get('/courses/total-situation-per-period', totalSituationPerPeriodAction);
